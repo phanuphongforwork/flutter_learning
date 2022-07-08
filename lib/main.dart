eb820d5cb982668cadeee5994a86148d533b085c
Phanuphong Machai <phanuphong.forwork@gmail.com>
2022-07-08 16:20:59 +0700

    feat: demo2

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'ภานุพงศ์ 6401103077003',
+      home: Scaffold(
+          appBar: AppBar(title: Text('TITLE')),
+          body: Text(
+            'Body',
+            textDirection: TextDirection.ltr,
+          ))));
+}
